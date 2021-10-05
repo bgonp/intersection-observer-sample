b8e9ce08d26e20cb6c142830d97b050e20ecd418
Borja González <borja@bgon.es>
2021-10-05 15:59:19 +0200

    refactor: use custom refs can be used even on dynamic components

--- src/useVisibleListener.ts
+++ src/useVisibleListener.ts
@@ -1,31 +1,63 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 
-const TRESHOLD = 0;
+const THRESHOLD = 0;
+
+type VisibilityRef = {
+  parent: Element | null;
+  child: Element | null;
+  observer: IntersectionObserver | null;
+};
 
 const useVisibleListener = (
   callback: () => void,
   marginPercentage: number = 0
 ) => {
-  const parentRef = useRef<HTMLUListElement>(null);
-  const childRef = useRef<HTMLLIElement>(null);
+  const elementsRef = useRef<VisibilityRef>({
+    parent: null,
+    child: null,
+    observer: null
+  });
 
-  useEffect(() => {
-    if (!parentRef.current || !childRef.current) return;
+  const observe = useCallback(() => {
+    const { parent, child, observer } = elementsRef.current;
+
+    if (observer) observer.disconnect();
+
+    if (!parent || !child) return;
+
     const options = {
-      root: parentRef.current,
+      root: parent,
       rootMargin: `${marginPercentage}%`,
-      treshold: TRESHOLD
+      threshold: THRESHOLD
     };
+
     const triggeredAction = ([entry]: IntersectionObserverEntry[]) =>
       entry.isIntersecting && callback();
-    const observer = new IntersectionObserver(triggeredAction, options);
 
-    observer.observe(childRef.current);
+    const nextObserver = new IntersectionObserver(triggeredAction, options);
+    nextObserver.observe(child);
 
-    return () => observer.disconnect();
+    elementsRef.current = { parent, child, observer: nextObserver };
   }, [callback, marginPercentage]);
 
-  return { parentRef, childRef };
+  const getRef = useCallback(
+    (key: "parent" | "child") => (element: Element | null) => {
+      if (element === elementsRef.current[key]) return;
+      elementsRef.current = { ...elementsRef.current, [key]: element };
+      observe();
+    },
+    [observe]
+  );
+
+  const visibiltyRefs = useMemo(
+    () => ({
+      parentRef: getRef("parent"),
+      childRef: getRef("child")
+    }),
+    [getRef]
+  );
+
+  return visibiltyRefs;
 };
 
 export default useVisibleListener;
