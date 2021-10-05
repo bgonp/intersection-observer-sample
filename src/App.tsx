d30d0a35de319d7759d8b7369c9608b4158a9e57
Borja González <borja@bgon.es>
2021-10-05 09:29:35 +0200

    feat: create useVisibleListener

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
+import useInfiniteScroll from "./useInfiniteScroll";
+
 import "./styles.css";
 
+const ELEMS = 100;
+
+const getRandomString = () => Math.random().toString(36).split(".")[1];
+
 export default function App() {
+  const { parentRef, childRef } = useInfiniteScroll(() =>
+    console.log("triggered")
+  );
+
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <ul ref={parentRef}>
+        {[...Array(ELEMS)].map((_, i) => {
+          if (i < ELEMS - 1) return <li key={i}>{getRandomString()}</li>;
+          return (
+            <li key={i} ref={childRef}>
+              {getRandomString()}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ src/useVisibleListener.ts
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from "react";
+
+const TRESHOLD = 0;
+
+const useVisibleListener = (
+  callback: () => void,
+  marginPercentage: number = 0
+) => {
+  const parentRef = useRef<HTMLUListElement>(null);
+  const childRef = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    if (!parentRef.current || !childRef.current) return;
+    const options = {
+      root: parentRef.current,
+      rootMargin: `${marginPercentage}%`,
+      treshold: TRESHOLD
+    };
+    const triggeredAction = ([entry]: IntersectionObserverEntry[]) =>
+      entry.isIntersecting && callback();
+    const observer = new IntersectionObserver(triggeredAction, options);
+
+    observer.observe(childRef.current);
+  }, [callback, marginPercentage]);
+
+  return { parentRef, childRef };
+};
+
+export default useVisibleListener;
